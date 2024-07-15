d5652ff560b59d545aa9c7de0f4d840f0260e159
claudiopatti <claudiopatti1997@gmail.com>
2024-07-15 17:51:07 +0200

    creati badge per le caselle

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
-console.log('ciao console')
+console.log('ciao console');
+
+// const myRow = document.querySelector('row');
 
 for (let i = 1; i <=100; i++) {
     // console.log('tabella :' + i )
+    const myRow = document.querySelector('.row');
 
     if (i % 5 == 0 && i % 3 == 0) {
-        console.log('tabella:' + 'FizzBuzz')
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML += 'FizzBuzz';
+        newDiv.classList.add('col' , 'badge', 'bg-danger' , 'p-4' , 'm-3')
+        myRow.append(newDiv);
+        console.log('tabella:' + 'FizzBuzz');
         
     }
     else if (i % 3 == 0) {
-        console.log('tabella:' + 'fizz')
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML += 'fizz';
+        newDiv.classList.add('col' , 'badge' , 'bg-success' , 'p-4' , 'm-3')
+        myRow.append(newDiv);
+        console.log('tabella:' + 'fizz');
         
     } 
     else if (i % 5 == 0) {
-        console.log('tabella:' + 'buzz')
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML += 'buzz';
+        newDiv.classList.add('col' , 'badge' , 'bg-warning' , 'p-4' , 'm-3')
+        myRow.append(newDiv);
+        console.log('tabella:' + 'buzz');
         
     } 
    
     else {
-        console.log('tabella :' + i )
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML += i;
+        newDiv.classList.add('col' , 'badge', 'bg-primary' , 'p-4' , 'm-3')
+        myRow.append(newDiv);
+        console.log('tabella :' + i );
     }
+
     
-
-
-
-    // calcolo multipli 
-    // if (x % y == 0) {
-    //     console.log()
-        
-    // }
+    
 }
