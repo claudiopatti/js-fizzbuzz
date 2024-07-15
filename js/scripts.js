25bca3d4237022b66472474216afe42f20c84a90
claudiopatti <claudiopatti1997@gmail.com>
2024-07-15 16:05:20 +0200

    calcolato come far scrivere in console con i multipli di 3 e di 5 o di tutti e 2 insieme

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
 console.log('ciao console')
+
+for (let i = 1; i <=100; i++) {
+    // console.log('tabella :' + i )
+
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log('tabella:' + 'FizzBuzz')
+        
+    }
+    else if (i % 3 == 0) {
+        console.log('tabella:' + 'fizz')
+        
+    } 
+    else if (i % 5 == 0) {
+        console.log('tabella:' + 'buzz')
+        
+    } 
+   
+    else {
+        console.log('tabella :' + i )
+    }
+    
+
+
+
+    // calcolo multipli 
+    // if (x % y == 0) {
+    //     console.log()
+        
+    // }
+}
